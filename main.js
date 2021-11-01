aeb2ff8dfb1ec0101e21411391615fc74de912fd
Lucian Kallee <luciankallee@gmail.com>
2021-11-01 12:00:14 +0200

    first commit

--- main.js
+++ main.js
@@ -32,14 +32,17 @@ torus.position.set(0, 0, -20);
 const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
 
+
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20,20,20);
+
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight,ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(lightHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 //stars
